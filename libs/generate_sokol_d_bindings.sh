4163a32a0a10b1f251eb55d52362207c4b641ebc
gilzoide <gilzoide@gmail.com>
2020-11-16 16:44:49 -0300

    Fix sokol_glue.d

--- libs/generate_sokol_d_bindings.sh
+++ libs/generate_sokol_d_bindings.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 pushd sokol
+
 dstep --alias-enum-members *.h -o ../sokol_d
 # Fix anonymous enum aliases like "alias SAPP_MAX_TOUCHPOINTS = .SAPP_MAX_TOUCHPOINTS;"
 sed -i '/alias \w* = \.\w*;/d' ../sokol_d/*.d
+
+cat > ../sokol_d/sokol_glue.d <<EOL
+import sokol_gfx;
+
+extern(C):
+
+sg_context_desc sapp_sgcontext();
+EOL
+
 popd
--- src/app.d
+++ src/app.d
@@ -2,8 +2,27 @@ import sokol_app;
 import sokol_gfx;
 import sokol_glue;
 
-extern(C) sapp_desc sokol_main(int argc, char **argv) {
+extern(C):
+
+void init()
+{
+    sg_desc desc = {
+        context: sapp_sgcontext(),
+    };
+    sg_setup(&desc);
+}
+
+void cleanup()
+{
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char **argv)
+{
     sapp_desc desc = {
+        init_cb: &init,
+        cleanup_cb: &cleanup,
+
         window_title: "olars",
     };
     return desc;
