dd66680d42da828ca7a8f6ddf7ecd31552976aab
gilzoide <gilzoide@gmail.com>
2020-11-16 17:48:11 -0300

    Add default pass_action, compile D and C source files together (Meson rocks!)

--- src/app.d
+++ src/app.d
@@ -2,6 +2,19 @@ import sokol_app;
 import sokol_gfx;
 import sokol_glue;
 
+import gl3n.math;
+
+struct Game {
+    sg_pass_action default_pass_action = {
+        colors: [{
+            action: SG_ACTION_CLEAR,
+            val: [0.2, 0.2, 0.2, 1],
+        }],
+    };
+}
+
+__gshared Game game;
+
 extern(C):
 
 void init()
@@ -10,6 +23,14 @@ void init()
         context: sapp_sgcontext(),
     };
     sg_setup(&desc);
+}
+
+void frame()
+{
+    int width = sapp_width(), height = sapp_height();
+    sg_begin_default_pass(&game.default_pass_action, width, height);
+    sg_end_pass();
+    sg_commit();
 }
 
 void cleanup()
@@ -21,6 +42,7 @@ sapp_desc sokol_main(int argc, char **argv)
 {
     sapp_desc desc = {
         init_cb: &init,
+        frame_cb: &frame,
         cleanup_cb: &cleanup,
 
         window_title: "olars",
