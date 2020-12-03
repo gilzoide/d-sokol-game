673220d61b9cff4e3b9f754f2690269a39d4b936
gilzoide <gilzoide@gmail.com>
2020-12-03 18:59:01 -0300

    Remove betterC default setup

--- setup-all.sh
+++ setup-all.sh
@@ -7,6 +7,5 @@ maybewipe() {
 }
 
 env DC=gdc meson setup $(maybewipe build)
-env DC=ldc meson setup $(maybewipe build/betterC)
 env DC=ldc meson setup --buildtype release $(maybewipe build/release)
 meson setup --cross-file cross-web.ini --buildtype release $(maybewipe build/web)
