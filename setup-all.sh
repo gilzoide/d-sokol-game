3383d7b593b92451f3ecfb4092f063a020e21486
gilzoide <gilzoide@gmail.com>
2020-12-09 20:17:01 -0300

    Add separate debug web build from release web build, mess with emscripten parameters

--- setup-all.sh
+++ setup-all.sh
@@ -8,4 +8,5 @@ maybewipe() {
 
 env DC=gdc meson setup $(maybewipe build)
 env DC=ldc meson setup --buildtype release $(maybewipe build/release)
-meson setup --cross-file cross-web.ini --buildtype release $(maybewipe build/web)
+meson setup --cross-file cross-web.ini $(maybewipe build/web)
+meson setup --cross-file cross-web.ini -Ddebug=false -Doptimization=s $(maybewipe build/release/web)
