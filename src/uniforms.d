ef2bb223ff4ee188f9bdf678f304d1439aac7d23
gilzoide <gilzoide@gmail.com>
2020-12-26 12:47:36 -0300

    Apply UniformPadding and alignment directly to Uniforms template struct, add soa subproject

--- src/uniforms.d
+++ src/uniforms.d
@@ -1,15 +1,18 @@
+import soa;
 import sokol_gfx;
 
 import mathtypes;
 
-struct Uniforms(T, int slot = 0, sg_shader_stage shader_stage = SG_SHADERSTAGE_VS)
+align(16) struct Uniforms(T, int slot = 0, sg_shader_stage shader_stage = SG_SHADERSTAGE_VS)
 {
     T uniforms;
     alias uniforms this;
 
+    mixin UniformPadding;
+
     void draw()
     {
-        sg_apply_uniforms(shader_stage, slot, &uniforms, T.sizeof);
+        sg_apply_uniforms(shader_stage, slot, &this, Uniforms.sizeof);
     }
 }
 
@@ -23,24 +26,24 @@ mixin template UniformPadding()
         byte[16 - T.sizeof % T.alignof] __pad;
     }
 }
-private align(16) struct CameraUniform_
+
+private struct CameraUniform_
 {
     Mat4 projection_matrix = Mat4.identity;
-    mixin UniformPadding;
 }
-private align(16) struct StandardUniform_
+private struct StandardUniform_
 {
     Transform3D transform = Transform3D.identity;
     Vec4 tint_color = 1;
-    mixin UniformPadding;
 }
-private align(16) struct UVTransformUniform_
+private struct UVTransformUniform_
 {
     Transform3D transform = Transform3D.identity;
-    mixin UniformPadding;
 }
+
+alias StandardInstancedUniform_ = SOA!(StandardUniform_, 16);
 
 alias CameraUniform = Uniforms!(CameraUniform_, 0);
 alias StandardUniform = Uniforms!(StandardUniform_, 1);
+alias StandardInstancedUniform = Uniforms!(StandardInstancedUniform_, 1);
 alias UVTransformUniform = Uniforms!(UVTransformUniform_, 2);
-
